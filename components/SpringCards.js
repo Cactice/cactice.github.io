e52627018311455eb91978e6ab4a4c470dfe1cc3
Cactice <14835424+Cactice@users.noreply.github.com>
2019-02-08 01:17:44 +0900

    fix card issues

--- components/SpringCards.js
+++ components/SpringCards.js
@@ -1,70 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { useSpring, animated } from 'react-spring'
 
 import {Button} from 'react-bootstrap'
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const calc = (x, y, size) => [-(y - size.y-(size.height/2)) / 10,(x-size.x-(size.width/2))/10, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-class ReportSize extends React.Component {
-  refCallback = element => {
-    if (element) {
-      this.setState({getSize:element.getBoundingClientRect()})
-      console.log(this.getSize,'ah')
-    }
-  };
- 
-  render() {
-    return (
-      <div>
-      <div ref={this.refCallback} style={{ border: "1px solid red" }}>
-        {this.state?this.state.getSize.x:<div/>}<br/>
-        {window.innerHeight}
-      </div>
-      <div>
-      <Button onClick={()=>{
-        console.log(this.state.getSize.x,'ah')
-      }}>afjkl</Button>
-      </div>
-      </div>
-    );
-  }
-}
 export default function SpringCards() {
+  const [size, setSize] = useState({x:0,y:0})
   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   let node = {}
+  let refCallback = element => {
+    if (element) {
+      node=element//
+    }
+  }
+  useEffect(() => {
+    setSize(node.getBoundingClientRect())
+  })
   return (
-    <div ref={ref => node = ref}>
+    <div ref={refCallback} >
     <animated.div
-      class="card"
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      className="card"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y, size) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
-      style={{ transform: props.xys.interpolate(trans) }}
+      style={{ transform: props.xys.interpolate(trans) , boxShadow:`0px 5px 30px 5px rgba(0, 0, 0, 0.1)`}}
     >
-    <ReportSize/>
-    <p>hai</p>
+    <p style={{height:200}}>hai</p>
     </animated.div>
     <style jsx>{`
-      root {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        background-color: white;
-      }
-
-      body {
-        font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu,
-          roboto, noto, segoe ui, arial, sans-serif;
-        background: transparent;
-        -webkit-touch-callout: none;
-        -webkit-user-select: none;
-        -khtml-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        cursor: default;
-      }
 
       root {
         display: flex;
@@ -75,17 +39,17 @@ export default function SpringCards() {
       }
 
       .card {
-        width: 45ch;
-        height: 45ch;
+        width: 15rem;
+        height: 450ch;
         background: grey;
         border-radius: 5px;
         background: url(https://drscdn.500px.org/photo/435236/q%3D80_m%3D1500/v2?webp=true&sig=67031bdff6f582f3e027311e2074be452203ab637c0bd21d89128844becf8e40);
+        box-shadow: 0px 10px 30px 5px rgba(0, 0, 0, 0.3);
         background-size: cover;
         background-position: center center;
-        box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.3);
         transition: box-shadow 0.5s;
         will-change: transform;
-        border: 15px solid white;
+        border: 35px solid white;
       }
 
       .card:hover {
