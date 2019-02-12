0986d6d94c5a7f8de9ee593e18573f2da7b3f859
Cactice <14835424+Cactice@users.noreply.github.com>
2019-02-12 17:28:18 +0900

    improve performance with tree shake on Threejs

--- components/Renderer.js
+++ components/Renderer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import  {WebGLRenderer} from 'three'
+import  {WebGLRenderer} from './threejs/src/renderers/WebGLRenderer.js'
 import ReactResizeDetector from 'react-resize-detector'
 
 
--- components/Scene.js
+++ components/Scene.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-import {Scene, ImageUtils, Mesh, PerspectiveCamera, Color, JSONLoader, ShaderMaterial, PointLight} from 'three'
+//import {Scene, TextureLoader, Mesh, PerspectiveCamera, Color, JSONLoader, ShaderMaterial, AmbientLight } from 'three'
+import {TextureLoader} from './threejs/src/loaders/TextureLoader.js'
+import {Mesh} from './threejs/src/objects/Mesh'
+import {PerspectiveCamera} from './threejs/src/cameras/PerspectiveCamera.js'
+import {Color} from './threejs/src/math/Color.js'
+import {JSONLoader} from './threejs/src/loaders/JSONLoader.js'
+import {ShaderMaterial} from './threejs/src/materials/ShaderMaterial'
+import {AmbientLight} from './threejs/src/lights/AmbientLight'
+import {Scene} from './threejs/src/scenes/Scene.js'
 import Renderer from './Renderer'
 
 
@@ -17,36 +25,48 @@ import {fragShader} from './threejs/fragShader.js'
  *
  * Functions of this class are to be passed as callbacks to Renderer.
  *
- * Parameters passed to every function are:
+ * Parameters passed to every function are: 
  *
- *   - renderer:  Three.js WebGLRenderer object
+ * - renderer: Three.js WebGLRenderer object
  *     (https://threejs.org/docs/#api/renderers/WebGLRenderer)
  *
- *   - gl:  WebGLRenderingContext of the underlying canvas element
+ * - gl: WebGLRenderingContext of the underlying canvas element
  *     (https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext)
  */
 export default class Scene2 extends React.Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
-      rotationDirection: +1, // shows which direction cube spins
+      rotationDirection: +1,   // shows which direction cube spins
     }
     this.start = Date.now()
+    this.scene = new Scene()
+
+    var loader = new TextureLoader()
+    loader.load('/static/threejs/UVface2.webp', (tex)=> {
+      this.uniforms = {
+        texture1: { type: 't', value: tex},
+        time    : { // float initialized to 0
+          type : 'f',
+          value: 0.0,
+        },
+      }
+      this.material = new ShaderMaterial({
+        uniforms      : this.uniforms,
+        vertexShader  : vertShader,
+        fragmentShader: fragShader,
+      })
+
+      let loader = new JSONLoader()
+      loader.load( '/static/threejs/me4.json', ( geometry, materials ) => {
+        let json = new Mesh( geometry, this.material)
+        json.position.set( 0,1,-1)
+        json.scale.set( 1, 1, 1 )
+        this.scene.add( json )
+      } )
+    })
   }
 
-  uniforms = {
-    texture1: { type: 't', value: ImageUtils.loadTexture('/static/threejs/UVface2.webp') },
-    time: { // float initialized to 0
-      type: 'f',
-      value: 0.0,
-    },
-  }
-
-  material = new ShaderMaterial({
-    uniforms: this.uniforms,
-    vertexShader: vertShader,
-    fragmentShader: fragShader,
-  })
 
   onResize = (renderer, gl, { width, height }) => {
     // This function is called after canvas has been resized.
@@ -56,30 +76,22 @@ export default class Scene2 extends React.Component {
   }
 
   initScene = (renderer, gl) => {
-    this.scene = new Scene()
 
-    let loader = new JSONLoader()
-    loader.load( '/static/threejs/me4.json', function ( geometry, materials ) {
-      let json = new Mesh( geometry, this.material)
-      json.position.set( 0,1,-1)
-      json.scale.set( 1, 1, 1 )
-      this.scene.add( json )
-    }.bind(this) )
+    const ambientLight = new AmbientLight('#ffffff', 0.5)
+    this.scene.add(ambientLight)
 
-    const pointLight = new PointLight('#ffffff', 1.0)
-    pointLight.position.set(5, 10, 5)
-    this.scene.add(pointLight)
-    this.scene.background =  new Color( 0xf0f0f0 )
+    this.scene.background = new Color( 0xf0f0f0 )
 
-    this.camera = new PerspectiveCamera(75, 16 / 9, 0.1, 1000)
+    this.camera            = new PerspectiveCamera(75, 16 / 9, 0.1, 1000)
     this.camera.position.z = 4
 
     renderer.setClearColor('#0d0d1e')
   }
 
   renderScene = (renderer, gl) => {
-
-    this.material.uniforms[ 'time' ].value = .00025 * ( Date.now() - this.start );
+    if(typeof this.material !== 'undefined'){
+      this.material.uniforms[ 'time' ].value = .00025 * ( Date.now() - this.start )
+    }
     renderer.render(this.scene, this.camera)
   }
 
@@ -97,16 +109,16 @@ export default class Scene2 extends React.Component {
           <div className='col-12'>
             <div className="row" >
               <Renderer
-                onResize={this.onResize}
-                initScene={this.initScene}
-                renderScene={this.renderScene}
+                onResize    = {this.onResize}
+                initScene   = {this.initScene}
+                renderScene = {this.renderScene}
               />
             </div>
           </div>
         </div>
         <style jsx>{`
           .container-fluid {
-            width: 100vw;
+            width : 100vw;
             height: 100vh;
           }
 
